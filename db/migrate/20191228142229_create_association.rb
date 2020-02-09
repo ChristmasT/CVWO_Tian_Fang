9fd9e5bd32d14f4cb28df097cbbcc05eb711c6dd
ChristmasT <tianfang2001@gmail.com>
2020-02-09 18:18:57 +0800

    Delete 3 migration files

--- db/migrate/20191228142229_create_association.rb
+++
@@ -1,8 +0,0 @@
-class CreateAssociation < ActiveRecord::Migration[6.0]
-  def change
-    create_table :tags_tasks, id: false do |t|
-      t.belongs_to :task
-      t.belongs_to :tag
-    end
-  end
-end
